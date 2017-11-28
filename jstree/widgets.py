61d84f9f82a76e370064f5fde9cd27b4acd88af8
J-E Castagnede <j.e.castagnede@gmail.com>
2017-11-28 17:09:51 +0100

    add option to hide input result

--- jstree/widgets.py
+++ jstree/widgets.py
@@ -6,14 +6,26 @@ from django.conf import settings
 
 
 class JsTreeWidget(TextInput):
-    def __init__(self, url, attrs=None):
+    def __init__(self, url, result_hidden=False, attrs=None):
+        """
+        JsTreePathFile Doc usage
+        :param url: URL will serve AJAX results. See https://www.jstree.com/docs/json/
+        :param result_hidden: Hide readonly input which contains selected result
+        :param attrs: Override django widget HTML attributes
+        """
         super(JsTreeWidget, self).__init__(attrs=attrs)
         self.url = url
-        # custom widget if not specified in attrs
-        self.attrs.setdefault('readonly', True)
-        self.attrs.setdefault('required', True)
-        self.attrs.setdefault('max_length', 255)
-        self.attrs.setdefault('size', 100)
+
+        if result_hidden:
+            self.input_type = 'hidden'
+            self.template_name = 'django/forms/widgets/hidden.html'
+
+        else:
+            # custom widget if not specified in attrs
+            self.attrs.setdefault('readonly', True)
+            self.attrs.setdefault('required', True)
+            self.attrs.setdefault('max_length', 255)
+            self.attrs.setdefault('size', 100)
 
     class Media:
         css = {
@@ -28,15 +40,15 @@ class JsTreeWidget(TextInput):
     def render(self, name, value, attrs=None, renderer=None):
         rendering = super(JsTreeWidget, self).render(name, value, attrs=attrs)
         div_id = "{}-tree".format(name)
+        # add custom HTML div
+        template = get_template("diva_monitor/widgets/jstree.div.html")
         rendering = "{}{}".format(rendering,
-                                  get_template("jstree/jstree.div.html").render(
-                                      {'div_id': div_id,
-                                       'field_name': name, })
-                                  )
+                                  template.render({'div_id': div_id,
+                                                   'field_name': name, }))
+        # add custom JS script
+        template = get_template("diva_monitor/widgets/jstree.init.js")
         rendering = "{}{}".format(rendering,
-                                  get_template("jstree/jstree.init.js").render(
-                                      {'div_id': div_id,
-                                       'field_name': name,
-                                       'url': self.url})
-                                  )
+                                  template.render({'div_id': div_id,
+                                                   'field_name': name,
+                                                   'url': self.url, }))
         return rendering
