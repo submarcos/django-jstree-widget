10bc3ca334392af52e74dbfcdffbf844918da214
J-E Castagnede <j.e.castagnede@gmail.com>
2017-11-28 12:08:00 +0100

    add MANIFEST.in for packaging

--- jstree/__init__.py
+++ jstree/__init__.py
@@ -1,2 +1,7 @@
-with open('../VERSION', 'r') as file_version:
+from __future__ import unicode_literals
+import os
+
+dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+with open(os.path.join(dir_path, 'VERSION'), 'r') as file_version:
     __version__ = file_version.read()
